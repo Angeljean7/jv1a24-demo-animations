80e67d2dcd58e231badada79f614c49f225fbc3c
Angeljean7 <xwars777@gmail.com>
2024-10-21 17:07:26 -0400

    Le joueur est vivant

---
+++ Assets/DoorController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter()
+    {
+        if(other.CompareTag("Player")){
+
+        }
+    }
+}
+    private void OnTriggerExit(){
+    {
+        if(other.CompareTag("Player")){
+
+        }
+    }
+}
